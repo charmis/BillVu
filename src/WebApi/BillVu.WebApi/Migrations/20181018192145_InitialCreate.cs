dc93238e0aa62ad74812cc541eefd64b19592167
Charmis Varghese <charmis@users.noreply.github.com>
2018-10-18 16:11:28 -0400

    Added Bill Controller, EF(SQLite)

---
+++ src/WebApi/BillVu.WebApi/Migrations/20181018192145_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BillVu.WebApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bills",
+                columns: table => new
+                {
+                    Name = table.Column<string>(nullable: false),
+                    ServiceProviderName = table.Column<string>(nullable: false),
+                    Amount = table.Column<double>(nullable: false),
+                    DueDate = table.Column<DateTime>(nullable: false),
+                    ReminderDate = table.Column<DateTime>(nullable: true),
+                    OnlinePayUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bills", x => new { x.Name, x.ServiceProviderName });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bills");
+        }
+    }
+}
---
+++ src/WebApi/BillVu.WebApi/Models/Bill.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BillVu.WebApi.Models
+{
+    public class Bill
+    {
+        [Key, Column(Order = 0)]
+        public string Name { get; set; }
+
+        [Key, Column(Order = 1)]
+        public string ServiceProviderName { get; set; }
+
+        public double Amount { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public DateTime? ReminderDate { get; set; }
+
+        public string OnlinePayUrl { get; set; }
+    }
+}
